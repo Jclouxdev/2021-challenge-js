890d5774547bd64371cce07d26d0c23e3e65e293
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-05 18:01:03 +0200

    Commit

--- sign.js
+++ sign.js
@@ -13,8 +13,8 @@ const sign = (a) => {
 }
 
 const sameSign = (a, b) => {
-  if (a > 0 && b > 0 || a < 0 && b < 0) { return true }
+  if (a > 0 && b > 0 || a < 0 && b < 0 || a === 0 && b === 0) { return true }
   else return false
 }
 
-//console.log(sameSign(0, 5))
+//console.log(sameSign(-0, -0))
