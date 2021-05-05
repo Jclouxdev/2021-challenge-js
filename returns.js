386c544132e7a2261dcaff82ecb69906cdb32732
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-05 14:39:34 +0200

    Commit

---
+++ returns.js
@@ -0,0 +1,10 @@
+/*
+id that takes one argument and return it
+getLength that takes an array or a string and return its length
+*/
+const arr = [0,2,3,4,5]
+
+const id = (n) => n
+const getLength = (m) => m.length
+
+//console.log(getLength(arr))
