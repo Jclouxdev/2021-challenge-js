1a71be3c00611b1573660e0fdfb02f7716f8ceee
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-05 11:55:02 +0200

    Commit

---
+++ declarations.js
@@ -0,0 +1,9 @@
+const escapeStr = '`'+'`'+'/'+'"'+"'"
+const arr = [4,2]
+const obj = {
+  str: 'String',
+  num: 1234,
+  bool: true,
+  undef: undefined
+}
+const nested = [4, undefined, '2'] + {str: "String", num: 1, bool: false}
