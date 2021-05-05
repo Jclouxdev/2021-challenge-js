ca0e7ec5900b9e25358b413fc81cf792b4068e38
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-05 14:29:27 +0200

    Commit

---
+++ more-or-less.js
@@ -0,0 +1,11 @@
+/*
+more that takes 1 argument and add 1 to it
+less that takes 1 argument and substract 1 to it
+add that takes 2 arguments and add them
+sub that takes 2 arguments and substract them
+*/
+
+const more = (n) => n + 1
+const less = (m) => m - 1
+const add = (e, f) => e + f
+const sub = (g, h) => g - h
