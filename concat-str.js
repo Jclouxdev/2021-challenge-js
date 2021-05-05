3f2a4b5b1345c12138b63eeb3a95810581456c16
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-05 15:06:19 +0200

    Commit

---
+++ concat-str.js
@@ -0,0 +1,8 @@
+//Create a concatStr function that takes 2 arguments and concatenate them
+
+const one = 2
+const two = 4
+const concated = ''
+const concatStr = (a, b) => concated.concat(a, b); 
+
+//console.log(concatStr(one, two))
