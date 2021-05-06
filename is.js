94434181c1db48b6940a8555da8e69f8f8dd3ae2
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-06 10:15:47 +0200

    Commit

--- is.js
+++ is.js
@@ -14,6 +14,87 @@ is.truthy value is truthy
 is.falsy value is falsy
 */
 
-const properties = (n) => typeof n
+/*const type = undefined
+const is = undefined
+const properties = (is, type) => {
+  if(typeof is === 'number'){
+    type = 'is.num'
+  }
+  else if(typeof is === "NaN"){
+    
+  }
+}*/
+//const num = (n) => { if(typeof n === 'number') {return true} else return false }
 
-//console.log(properties('Test'))
+/*const is = {}
+const element = is
+is.num = (element) => {
+  if(typeof element === 'number'){return true} 
+  else return false
+}
+
+is.nan = (element) => {
+  if(typeof element === 'number'){return true} 
+  else return false
+}
+
+is.srt = (element) => {
+  if(typeof element === 'string'){return true} 
+  else return false
+}
+
+is.bool = (element) => {
+  if(typeof element === 'boolean'){ return true }
+  else return false
+}
+
+is.undef = (element) => {
+  if(typeof element === 'undefined'){ return true }
+  else return false
+}
+
+is.def = (element) => {
+  if(typeof element === 'defined'){ return true }
+  else return false
+}
+
+is.arr = (element) => {
+  if(typeof element === 'array'){ return true }
+  else return false
+}
+
+is.obj = (element) => {
+  if(typeof element === 'object'){ return true }
+  else return false
+}
+
+is.fun = (element) => {
+  if(typeof element === 'function'){ return true }
+  else return false
+}
+
+is.truthy = (element) => {
+  if(typeof element === 'truthy'){ return true }
+  else return false
+}
+
+is.falsy = (element) => {
+  if(typeof element === 'falsy'){ return true }
+  else return false
+}*/
+
+const is = {}
+is.num = (el) => typeof el === 'number';
+is.nan = (el) => isNaN(el);
+is.str = (el) => typeof el === 'string';
+is.bool = (el) => typeof el === 'boolean';
+is.undef = (el) => typeof el === 'undefined';
+is.def = (el) => !!el;
+is.arr = (el) => Array.isArray(el);
+is.obj = (el) => typeof el === 'object';
+is.fun = (el) => typeof el === 'function';
+is.truthy = (el) => typeof el === 'truthy';
+is.falsy = (el) => typeof el === 'falsy';
+
+console.log(is)
+console.log(is.arr([1,2]))
