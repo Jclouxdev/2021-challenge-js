55285b3e5ec967e99b2af7054823d52be3367a49
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-06 10:35:58 +0200

    Commit

---
+++ repeat.js
@@ -0,0 +1,14 @@
+/*
+Create a repeat function that takes a string and a number as parameters and return the repeated string by the 
+given number Like the method developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat 
+Of course you may not use the method directly
+*/
+
+var str = ''
+const repeat = (size, text) => {
+  for (let i = 0; i < size; i++) {
+    str = str + text
+  } return str
+}
+
+console.log(repeat(5, 'Test'))
