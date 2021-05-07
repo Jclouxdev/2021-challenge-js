dfaa26fb1a555326d091f09fbe25c30248b50f55
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-07 16:01:15 +0200

    Commit

--- cut-corners.js
+++ cut-corners.js
@@ -25,10 +25,12 @@ const round = (value) => {
   let answer = 0;
   let toRound = value % 1;
   if (toRound > 0.5) {
-    answer = value - toRound + 1
-  }
-  else {
+    answer = value + 1 - toRound
+  } else {
     answer = value - toRound
+  }
+  if (value < 0) {
+    answer = -answer
   }
   return answer
 }
