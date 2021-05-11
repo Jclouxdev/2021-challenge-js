831d73de61922fa1550505c42503423ff90903a3
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-11 09:22:33 +0200

    Commit

--- letter-space-number.js
+++ letter-space-number.js
@@ -6,16 +6,16 @@ if that number has only one digit and is not followed
 by any letter.
 */
 
-const letterSpaceNumber = (str) => {
+function letterSpaceNumber(str){
   const regex = /[a-zA-Z]\s\d\b/g
   const answer = str.match(regex)
-  if(answer != null) {
-    for(let i = 0; i<answer.length; i++) {
-      // answer[i] = answer[i].slice(0, answer[i].length - 1)
-    }
-  } else { 
-    const answer = []
-    return answer
+  if (answer != null){
+      for(let i = 0;i<answer.length;i++){
+          // res[i] = res[i].slice(0,res[i].length-1)
+      }
+  }else {
+      const answer = []
+      return answer
   }
   return answer
 }
