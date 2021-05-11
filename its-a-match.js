aafaa1408e99ec466b2523b4d13c00ea48f479a1
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-11 09:15:45 +0200

    Commit

--- its-a-match.js
+++ its-a-match.js
@@ -8,6 +8,16 @@ begin matches with the expression 'hi', only when it is in the beginning.
 end matches with the expression 'hi', only when it is in the end.
 
 beginEnd matches with the expression 'hi', only when it is in the beginning and the end.
+
+
+In regular expressions, we use anchors to check if the 
+matching symbol is the starting symbol or ending symbol 
+of the input string. Anchors are of two types: 
+The first type is the caret ^ that checks if the 
+matching character is the first character of the 
+input and the second type is the dollar sign $ which 
+checks if a matching character is the last character 
+of the input string.
 */
 
 const normal = /hi/
---
+++ letter-space-number.js
@@ -0,0 +1,20 @@
+/*
+Make a function called letterSpaceNumber that receives 
+a string and returns an array with every instance of a 
+letter, followed by a space, followed by a number only 
+if that number has only one digit and is not followed 
+by any letter.
+*/
+
+const letterSpaceNumber = (str) => {
+  const regex = /[a-zA-Z]\s\d\b/g
+  const answer = str.match(regex)
+  if(answer != null) {
+    for(let i = 0; i<answer.length; i++) {
+      answer[i] = answer[i].slice(0, answer[i].length - 1)
+    }
+  } else { 
+    return answer = []
+  }
+  return answer
+}
