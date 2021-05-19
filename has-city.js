eea84cb10abaafe04eab70bc9689d92e7b9f9eb6
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-19 10:06:36 +0200

    has-city

---
+++ has-city.js
@@ -0,0 +1,16 @@
+/*Create a function hasCity that takes a country and an array of cities of that country 
+and it returns a new function.
+The new function takes a city parameter which returns whether a city is part of that 
+country or not.
+
+If it is, it should be returned <city> is a city from <country>, otherwise <city> is not 
+a city from <country>.*/
+const hasCity = (country, array) =>
+  newFunction = (city) => {
+    for(let i = 0; i < array.length; i++){
+      if(array[i] === city){
+        return `${city} is a city from ${country}`
+      }
+    }
+    return `${city} is not a city from ${country}`
+  }
