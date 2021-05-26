31b469f11fe361c430d3f71852be7031641b46ec
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-26 11:47:07 +0200

    pick-omit.js

--- pick-omit.js
+++ pick-omit.js
@@ -4,26 +4,48 @@
 //This function create a new object that pick only the keys, in the second argument, 
 //from the object passed as argument.
 const pick = (obj, keys) => {
-  const myObject = {}
-  let getKeys = Object.keys(obj)
-  getKeys.forEach(key => {
-    if(key == keys){
-      myObject[key] = obj[key]
+  return keys.reduce((acc, k) => {
+    if (obj.hasOwnProperty(k)) {
+      return { ...acc, [k]: obj[k] };
     }
-  })
-  return myObject
-}
+    return acc;
+  }, {});
+};
 
 //Create a omit function that takes an object and keys. 
 //This function create a new object that omit only the keys, in the second argument, 
 //from the object passed as argument.
 const omit = (obj, keys) => {
-  const myObject = {}
-  let getKeys = Object.keys(obj)
-  getKeys.forEach(key => {
-    if(key != keys){
-      myObject[key] = obj[key]
-    }
-  })
-  return myObject
+  const alteredObj = obj;
+  keys.map((k) => delete alteredObj[k]);
+  return alteredObj;
+};
+
+
+//TEST
+const setupTest = {
+  agent: {
+    firstName: 'James',
+    lastName: 'Bond',
+    age: 25,
+    email: 'jamesbond@hotmail.com',
+  },
+  newAgent: { firstName: 'James', lastName: 'Bond' },
+  car: { brand: 'ford', motor: 'v8', year: 2000 },
+  newCar: { brand: 'ford', year: 2000 },
+  user: { firstName: 'John', lastName: 'Doe', age: 32, ageVerified: false },
+  newUser: { ageVerified: false },
+  computer: { brand: 'lenovo', ram: '32GB', processor: 'i7 8th Gen' },
+  tools: { drill: 'bosh', grinders: 'DeWalt', saws: ' Makita' },
+  newtool: { drill: 'bosh' },
+  games: { board: 'monopoly', cards: 'poker', dice: 'roulette' },
+  newgames: { dice: 'roulette' },
+  language: { England: 'english', Spain: 'spanish', Portugal: 'portuguese' },
+  newlanguage: { England: 'english', Portugal: 'portuguese' },
+  phone: { samsung: 'galaxy', asus: 'zenphone', nokia: 'lumia' },
 }
+console.log(pick(setupTest.agent, ['firstName', 'lastName']))
+console.log('->', setupTest.newAgent)
+
+console.log((omit(setupTest.tools, ['grinders', 'saws'])))
+console.log('->', setupTest.newtool)
