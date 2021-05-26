5ee76ac905b27c7f5ec82a69511bdb1fa94dde30
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-26 10:39:28 +0200

    pick-omit.js

---
+++ pick-omit.js
@@ -0,0 +1,27 @@
+//<!>Those functions are pure and must not modify the given object<!>
+
+//Create a pick function that takes an object and keys. 
+//This function create a new object that pick only the keys, in the second argument, 
+//from the object passed as argument.
+const pick = (obj, keys) => {
+  const myObject = {}
+  Object.keys(obj).forEach(key => {
+    if(key === keys){
+      myObject[key] = obj[key]
+    }
+  })
+  return myObject
+}
+
+//Create a omit function that takes an object and keys. 
+//This function create a new object that omit only the keys, in the second argument, 
+//from the object passed as argument.
+const omit = (obj, keys) => {
+  const myObject = {}
+  Object.keys(obj).forEach(key => {
+    if(key !== keys){
+      myObject[key] = obj[key]
+    }
+  })
+  return myObject
+}
