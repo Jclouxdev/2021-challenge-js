737c108504f462fcb14ad2b1f3d54dd78f680ee2
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-05-27 10:32:05 +0200

    fusion.js

---
+++ fusion.js
@@ -0,0 +1,53 @@
+/*The objective of this exercise is to merge objects into a new object depending on the values type
+With this create a function called fusion that:
+If the type is an array you must concat it
+fusion([1, 2], [3, 4]) // -> [1,2,3,4]
+
+If it is a string you must concatenate with a space
+fusion('Salem', 'alem') // -> 'Salem alem'
+
+If it is numbers you must added them
+fusion(4, 11) // -> 15
+
+In case of type mismatch you must replace it with the value of the second object
+fusion({ a: 'hello', b: [] }, { a: 4 })
+// -> { a: 4, b: [] }
+
+If it is an object you must fusion them recursively
+fusion({ a: 1, b: { c: 'Salem' } }, { a: 10, x: [], b: { c: 'alem' } })
+// -> { a: 11, x: [], b: { c: 'Salem alem' } }*/
+const fusion = (el1, el2) => {
+  // type mismatch, replace value in first by second value
+  if (typeof el1 !== typeof el2) {
+    return el2;
+  }
+
+  // type object, call fusion recursively to fusion properties
+  if (typeof el1 === "object" && !Array.isArray(el1)) {
+    const el1Buffer = {};
+    for (const [key] of Object.entries(el1)) {
+      el1Buffer[key] = fusion(el1[key], el2[key]);
+    }
+    return el1Buffer;
+  }
+
+  if (Array.isArray(el1) && Array.isArray(el2)) {
+    return el1.concat(el2);
+  }
+
+  if (typeof el1 === "string" && typeof el2 === "string") {
+    return `${el1} ${el2}`;
+  }
+
+  if (typeof el1 === "number" && typeof el2 === "number") {
+    return el1 + el2;
+  }
+};
+
+//TEST
+console.log(fusion({ arr: [1, '2'] }, { arr: [2] }),'->', [1, '2', 2])
+console.log(fusion('Ceci est', 'un test'),'->', 'Ceci est un test')
+console.log(fusion(25, 19),'->', 44)
+console.log(
+  fusion(Object.freeze({ a: { b: 1 } }), Object.freeze({ a: { b: 2 } }))
+);
