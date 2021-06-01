f1aeb7d387a7aba77a2c78ecd05d35ac49b8dc57
Joseph Cloux <josephcloux@MacBook-Pro-de-Joseph.local>
2021-06-01 09:37:50 +0200

    interpolation.js

---
+++ interpolation.js
@@ -0,0 +1,19 @@
+/*Create a function called interpolation that takes an object with 5 properties : 
+step, start, end, callback and duration. This function must calculate the interpolation points, 
+(x, y), from the start position to end position depending on the number of steps. 
+All the points must be calculated in the duration time.
+
+For each interpolation point you must call callback function with 
+parameter - interpolation point ([x, y]). Each interpolation point should be calculated 
+with interval of duration / step.*/
+
+const interpolation = ({step, start, callback, duration}) => {
+  let x = start
+  let y = duration / step
+  let timer = setInterval(() => {
+    callback([x, y]);
+    x += (end - start) / step
+    y += duration / step
+    if(Math.round(x * 100) / 100 === end) clearInterval(timer);
+  }, duration / step);
+}
